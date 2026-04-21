113e938c7b0abbc86a728a1612bb11e5d011fe19
PuPeepPuPeep <cjsonastv@gmail.com>
2026-04-22 04:06:57 +0700

    chore: store allowed_origins in env

--- backend/main.py
+++ backend/main.py
@@ -3,20 +3,27 @@ from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.jobs.scheduler import scheduler
 from app.api.routes import router
+import os
+import logging
+
+origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     scheduler.start()
-    print("Scheduler started!")
+    logger.info("Scheduler started!")
     yield
     scheduler.shutdown()
-    print("Scheduler shut down!")
+    logger.info("Scheduler shut down!")
 
 app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
