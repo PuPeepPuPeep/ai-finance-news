c72c043d0f972f4bc82d469d13837a75108f22f3
PuPeepPuPeep <cjsonastv@gmail.com>
2026-04-24 05:38:07 +0700

    fix: fix topic with special character by use params

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,16 +3,10 @@ import axios from "axios"
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const getNews = async (topic) => {
-    const url = (topic && topic !== 'All')
-    ? `${API_BASE_URL}/news?topic=${topic}`
-    : `${API_BASE_URL}/news`
-
-    const response = await axios.get(url)
-    return response.data
+    const config = {
+        params: (topic && topic !== 'Latest') ? { topic: topic} : {}
 }
-
-export const getNewsDetail = async (id) => {
-    const response = await axios.get(`${API_BASE_URL}/news/${id}`)
+    const response = await axios.get(`${API_BASE_URL/news}`, config)
     return response.data
 }
 
