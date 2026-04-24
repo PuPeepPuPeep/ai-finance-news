29e0c4d23f44e841d23af07a35b81d1c260ae6e3
PuPeepPuPeep <cjsonastv@gmail.com>
2026-04-25 03:30:23 +0700

    refactor: add try/catch to getNews

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,12 +3,16 @@ import axios from "axios"
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const getNews = async (topic) => {
-    const config = {
-        params: (topic && topic !== 'All') ? { topic: topic } : {}
+    try {
+        const config = {
+            params: (topic && topic !== 'All') ? { topic: topic } : {}
+        }
+        const response = await axios.get(`${API_BASE_URL}/news`, config)
+        return response.data
+    } catch (error) {
+        console.error("Error fetching news:", error)
+        return []
     }
-    
-    const response = await axios.get(`${API_BASE_URL}/news`, config)
-    return response.data
 }
 
 export const getTimeSummary = async () => {
